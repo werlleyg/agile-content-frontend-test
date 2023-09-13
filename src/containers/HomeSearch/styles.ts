ad18e15c156475fbd2ab1061ccc64adf06174e16
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-09-13 19:09:48 -0300

    feat: centralize google image

--- src/containers/HomeSearch/styles.ts
+++ src/containers/HomeSearch/styles.ts
@@ -1,5 +1,8 @@
-import styled from 'styled-components';
+import styled from "@emotion/styled";
 
 export const Container = styled.div`
-
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
