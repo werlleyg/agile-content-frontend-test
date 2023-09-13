17f276411b7fd8e3bf807fba0f91ea2dd7415a84
Werlley Gonçalves <werlley.registros@hotmail.com>
2023-09-13 14:21:14 -0300

    feat: create Footer container

---
+++ src/containers/Footer/styles.ts
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--footer-color);
+  padding: 1rem 1.5rem;
+`;
+
+export const P = styled.p`
+  font-size: 0.875rem;
+  color: var(--text-color);
+`;
